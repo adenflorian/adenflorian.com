2df5d9c4725352c074dde3a03a4a780d5c083a16
David Valachovic <AdenFlorian@gmail.com>
2014-04-15 11:45:06 -0500

    Fixing game urls

--- games.php
+++ games.php
@@ -2,13 +2,13 @@
 
 	<h1>RoboEscape</h2>
 	<p>Hold space bar to use jetpack to avoid obstacles. Click speaker icon to turn music off.</p>
-	<a href="https://dl.dropboxusercontent.com/u/160588973/unityprojects/endlessrunnera/builds/Current/Current.html" target="_blank">
+	<a href="https://dl.dropboxusercontent.com/u/160588973/unityprojects/endlessrunnera/builds/Current/RoboEscape.html" target="_blank">
 		<img src="img/roboescape.png" width="720"/>
 	</a>
 
 	<h1>Robotronic</h2>
 	<p>WASD to move and arrow keys to shoot. Only 6 levels. Refresh page to start over.</p>
-	<a href="http://www.adenflorian.com/Robotronic/Robotronic_v0.html" target="_blank">
+	<a href="https://dl.dropboxusercontent.com/u/160588973/unityprojects/robotronic/Current/Robotronic.html" target="_blank">
 		<img src="img/robotronic.png" width="720"/>
 	</a>
 

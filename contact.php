869baa9f11b3f2941ae21f02260b5b481eaa4f9b
David Valachovic <AdenFlorian@gmail.com>
2014-04-27 17:46:37 -0500

    Removing phoenix and swatches

--- contact.php
+++ contact.php
@@ -7,7 +7,6 @@ Author: David "AdenFlorian" Valachovic
 	<p>My name is David Valachovic, but I mostly go by AdenFlorian online.</p>
 	<p>I make games for all platforms with <a href="http://unity3d.com/" target="_blank">Unity</a> using C# scripts.</p>
 	<p>I make websites using HTML, CSS, JavaScript (<a href="http://jquery.com/" target="_blank">jQuery</a>), PHP (<a href="http://cakephp.org/" target="_blank">CakePHP</a>), and MySQL.</p>
-	<p>I study computer science at the University of Phoenix.</p>
 	<p id="social-icons">
 		<a href="mailto:David.E.Valachovic@gmail.com" target="_blank">
 			<i class="fa fa-envelope fa-3x"></i>
@@ -24,7 +23,9 @@ Author: David "AdenFlorian" Valachovic
 		<a href="https://bitbucket.org/AdenFlorian" target="_blank">
 			<i class="fa fa-bitbucket fa-3x"></i>
 		</a>
-		<!--<p><a href="http://answers.unity3d.com/users/95640/adenflorian.html" target="_blank">Unity Answers</a></p>-->
+		<!--<a href="http://answers.unity3d.com/users/95640/adenflorian.html" target="_blank">
+			<i class="fa fa-bitbucket fa-3x"></i>
+		</a>-->
 	</p>
 
 </div>

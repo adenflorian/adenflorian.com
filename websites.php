ba1ca5e2e8a01d54d8cc46b7c3ada15a2717edda
David Valachovic <AdenFlorian@gmail.com>
2014-08-20 14:56:55 -0500

    changed imperium portal text

--- websites.php
+++ websites.php
@@ -5,7 +5,7 @@ Author: David "AdenFlorian" Valachovic
 <div id="websites" class="right-content">
 
 	<h1>Imperium Portal</h1>
-	<p>In development since Fall 2013. I joined the team in February of 2014.</p>
+	<p>In development since Fall 2013. Became lead developer in April of 2014.</p>
 	<a href="http://portal.starcitizenbase.com/" target="_blank">
 		<img class="banner" src="img/imperiumportal.png" alt="Imperium Portal screenshot"/>
 	</a>

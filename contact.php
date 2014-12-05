ad1ef6f0b9669c3081286cc6c2a9873fd7996bc2
David Valachovic <AdenFlorian@gmail.com>
2014-12-04 21:25:30 -0800

    Edited contact info

--- contact.php
+++ contact.php
@@ -4,9 +4,10 @@ Author: David "AdenFlorian" Valachovic
 
 <div id="contact" class="right-content">
 
-	<p>My name is David Valachovic, but I mostly go by AdenFlorian online.</p>
-	<p>I make games for all platforms with <a href="http://unity3d.com/" target="_blank">Unity</a> using C# scripts.</p>
-	<p>I make websites using HTML, CSS, JavaScript (<a href="http://jquery.com/" target="_blank">jQuery</a>), PHP (<a href="http://cakephp.org/" target="_blank">CakePHP</a>), and MySQL.</p>
+	<p>My name is David Valachovic.</p>
+	<p>I make games for all platforms with <a href="http://unity3d.com/" target="_blank">Unity</a> using C# and JavaScript.</p>
+	<p>I make websites using HTML, CSS, JavaScript (<a href="http://jquery.com/" target="_blank">jQuery</a>), PHP (<a href="http://cakephp.org/" target="_blank">CakePHP</a>, <a href="http://laravel.com/" target="_blank">Laravel</a>), and MySQL.</p>
+	<p>I serve websites with Apache and nginx on Ubuntu and CentOS servers.</p>
 	<p id="social-icons">
 		<a href="mailto:David.E.Valachovic@gmail.com" target="_blank">
 			<i class="fa fa-envelope fa-3x"></i>

4d74614c497089c11dda0eedaed3f7a63ad1bc33
David Valachovic <AdenFlorian@gmail.com>
2015-01-10 14:02:55 -0800

    updated links to games

--- games.php
+++ games.php
@@ -6,7 +6,7 @@ Author: David "AdenFlorian" Valachovic
 
 	<h1>RoboEscape</h1>
 	<p>Hold space bar to use jetpack to avoid obstacles. Click speaker icon to turn music off.</p>
-	<a href="https://dl.dropboxusercontent.com/u/160588973/unityprojects/endlessrunnera/builds/Current/RoboEscape.html" target="_blank">
+	<a href="http://adenflorian.itch.io/jetbotrun" target="_blank">
 		<img class="banner" src="img/roboescape.png" alt="RoboEscape screenshot"/>
 	</a>
 
@@ -14,7 +14,7 @@ Author: David "AdenFlorian" Valachovic
 
 	<h1>Robotronic</h1>
 	<p>Short and simple arena shooter game.</p>
-	<a href="https://dl.dropboxusercontent.com/u/160588973/unityprojects/robotronic/Current/Robotronic.html" target="_blank">
+	<a href="http://adenflorian.itch.io/robotronic" target="_blank">
 		<img class="banner" src="img/robotronic.png" alt="Robotronic screenshot"/>
 	</a>
 

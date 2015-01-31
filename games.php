1e72a7b1e7c004be817c72d08b494288924aed58
AdenFlorian <AdenFlorian@gmail.com>
2015-01-30 16:03:40 -0800

    added Tankular to Games

--- games.php
+++ games.php
@@ -4,11 +4,19 @@ Author: David "AdenFlorian" Valachovic
 
 <div id="games" class="right-content">
 
-	<h1>RoboEscape</h1>
-	<p>Hold space bar to use jetpack to avoid obstacles. Click speaker icon to turn music off.</p>
-	<a href="http://adenflorian.itch.io/jetbotrun" target="_blank">
-		<img class="banner" src="img/roboescape.png" alt="RoboEscape screenshot"/>
-	</a>
+    <h1>Tankular</h1>
+    <p>WASD to drive, mouse to look, click to shoot.</p>
+    <a href="http://adenflorian.itch.io/tankgame" target="_blank">
+        <img class="banner" src="img/tankular.png" alt="Tankular screenshot"/>
+    </a>
+
+    <div class="vert-space-25"></div>
+
+    <h1>RoboEscape</h1>
+    <p>Hold space bar to use jetpack to avoid obstacles. Click speaker icon to turn music off.</p>
+    <a href="http://adenflorian.itch.io/jetbotrun" target="_blank">
+        <img class="banner" src="img/roboescape.png" alt="RoboEscape screenshot"/>
+    </a>
 
 	<div class="vert-space-25"></div>
 
